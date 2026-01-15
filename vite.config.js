8ee10f1a8cd61fdc99af5ab2d23274851162efb8
shaivyaa-do <shaivyaasharma04@gmail.com>
2026-01-16 00:05:33 +0530

    vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
     plugins: [react()],
     server: {
         port: 3000,
-        open: true
+        open: true,
+        host: true
     },
     resolve: {
         alias: {
